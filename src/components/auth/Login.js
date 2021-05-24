69984a5313c9a8b457a47d2f6d8f72f1c7c4c19b
PKLogger <senseicuong@gmail.com>
2021-05-24 11:04:46 +0700

    Add Login & Register Page

---
+++ src/components/auth/Login.js
@@ -0,0 +1,101 @@
+
+import { Link, Redirect } from "react-router-dom";
+import { useState } from "react";
+import axios from "axios";
+import Swal from 'sweetalert2';
+
+import "../../css/Login.css";
+
+const Login = ({ setUserLogin }) => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [islogin, setIslogin] = useState(false);
+  const [role, setRole] = useState('');
+  const [status, setStatus] = useState(false);
+  let msgErr;
+
+  const data = {
+    email,
+    password,
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    axios
+      .post("/api/auth/login", data)
+      .then((res) => {
+        console.log("login success", res.data);
+        localStorage.setItem("token", res.data.data.access_token);
+        localStorage.setItem("role", res.data.data.user.role);
+        setIslogin(true);
+        setRole(res.data.data.user.role)
+        setUserLogin(res.data.data.user);
+        Swal.fire({
+          icon: 'success',
+          text: 'Đăng nhập thành công!',
+        })
+      })
+      .catch((err) => {
+        setStatus(true);
+      });
+  };
+
+  if (islogin && role === null) {
+    return <Redirect to={"/"} />;
+  } else if (islogin && role === 1) {
+    return <Redirect to={"/admin"} />;
+  }
+  if (status) {
+    msgErr = "Nhập sai email hoặc password!";
+  } else {
+    msgErr = "";
+  }
+
+  return (
+    <div className="login">
+      <div className="login-header">
+        <h2 className="text-center w-100 font-weight-bold">Đăng Nhập</h2>
+      </div>
+      <p className="mb-0 ml-4 text-center" style={{ color: "red" }}>
+        {msgErr}
+      </p>
+      <div className="login-body my-3">
+        <form onSubmit={handleSubmit}>
+          <div className="form-group my-4">
+            <input
+              type="email"
+              className="form-control"
+              placeholder="Email"
+              required="required"
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </div>
+          <div className="form-group my-2">
+            <input
+              type="password"
+              className="form-control"
+              placeholder="Mật khẩu"
+              required="required"
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </div>
+          <div className="form-group my-4">
+            <button type="submit" className="btn btn-danger btn-block btn-lg">
+              Đăng nhập
+            </button>
+          </div>
+        </form>
+      </div>
+      <div className="login-footer">
+        <div>
+          <Link to="/user/forgot_password">Quên mật khẩu?</Link>
+        </div>
+        <div>
+          Bạn chưa có tài khoản? <Link to="/user/register">Đăng ký</Link>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
---
+++ src/components/auth/Register.js
@@ -0,0 +1,213 @@
+import axios from "axios";
+import React, { useState, useEffect } from "react";
+import { Link, Redirect } from "react-router-dom";
+import "../../css/Register.css";
+import Swal from 'sweetalert2';
+
+const Register = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [password_confirmation, setPassword_confirmation] = useState("");
+  const [status, setStatus] = useState(0);
+  const [nameErr, setNameErr] = useState(false);
+  const [emailErr, setEmailErr] = useState(false);
+  const [passworErr, setPasswordErr] = useState(false);
+  const [passworConfirmationErr, setPasswordConfirmationErr] = useState(false);
+  const [msgErr, setMsgErr] = useState('');
+
+  useEffect(() => {
+    validate();
+  }, [name, email, password, password_confirmation]);
+
+  const validateEmail = new RegExp("^[a-z0-9]+@gmail([\.])com$");
+
+  const validatePassword = new RegExp(
+    "^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*[!@$^%()])[A-Za-][a-zA-Z0-9!@$%^&()]{6,}$"
+  );
+  const validdateName = new RegExp("^[a-zA-Z0-9]{1,20}$");
+
+  const validate = () => {
+    if (name !== "" && !validdateName.test(name)) {
+      setNameErr(true);
+    } else {
+      setNameErr(false);
+    }
+    if (email !== "" && !validateEmail.test(email)) {
+      setEmailErr(true);
+    } else {
+      setEmailErr(false);
+    }
+    if (password !== "" && !validatePassword.test(password)) {
+      setPasswordErr(true);
+    } else {
+      setPasswordErr(false);
+    }
+    if (password_confirmation !== "" && password_confirmation !== password) {
+      setPasswordConfirmationErr(true);
+    } else {
+      setPasswordConfirmationErr(false);
+    }
+  };
+
+  const data = {
+    name,
+    email,
+    password,
+    password_confirmation,
+    role: 0
+  };
+  console.log(data);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    axios
+      .post("/api/auth/signup", data)
+      .then((res) => {
+        console.log(res);
+        setStatus(1);
+        Swal.fire({
+          icon: 'success',
+          text: 'Đăng ký thành công!',
+        })
+      })
+      .catch((err) => {
+        setStatus(err.response.status);
+        setMsgErr(err.response.data.message);
+      });
+  };
+
+  let Errmsg;
+
+  if (status === 1) {
+    return <Redirect to={"/user/login"} />;
+  } else if (status === 400 && msgErr === 'ER002') {
+    Errmsg = "Email đã tồn tại";
+  } else if (status === 400 && msgErr === 'ER003') {
+    Errmsg = "Tên người dùng đã tồn tại";
+  } else if (status === 401) {
+    Errmsg = "Email không khả dụng"
+  } else if (status === 423) {
+    Errmsg = "Tài khoản đã bị khóa"
+  } else {
+    Errmsg = "";
+  }
+
+  return (
+    <div className="register">
+      <div className="register-header">
+        <h2 className="text-center w-100 font-weight-bold">Đăng ký</h2>
+      </div>
+      <p className="err mb-1 text-center" style={{ color: "red" }}>
+        {Errmsg}
+      </p>
+      <div className="register-body my-3">
+        <form onSubmit={handleSubmit}>
+          <div className="form-group my-2">
+            <label>
+              Email:
+              <input
+                type="email"
+                placeholder="Enter email"
+                className="form-control"
+                name="email"
+                id="email"
+                onChange={(e) => setEmail(e.target.value)}
+                required
+              />
+              {emailErr ? (
+                <span style={{ color: "red" }}>Email sai định dạng</span>
+              ) : (
+                ""
+              )}
+            </label>
+          </div>
+          <div className="form-group my-2">
+            <label>
+              Tên người dùng:
+              <input
+                type="text"
+                placeholder="Enter name"
+                className="form-control"
+                name="name"
+                id="name"
+                onChange={(e) => setName(e.target.value)}
+                required
+              />
+              {nameErr ? (
+                <span style={{ color: "red" }}>Tên sai định dạng</span>
+              ) : (
+                ""
+              )}
+            </label>
+          </div>
+          <div className="form-group my-2">
+            <label>
+              Mật khẩu:
+              <input
+                type="password"
+                placeholder="Enter password"
+                className="form-control"
+                name="password"
+                id="password"
+                onChange={(e) => setPassword(e.target.value)}
+                required
+              />
+              {passworErr ? (
+                <span style={{ color: "red" }}>Mật khẩu sai định dạng (gồm ít nhất 1 chữ hoa, chữ thường, số và ký tự đặc biệt !@$^%())</span>
+              ) : (
+                ""
+              )}
+            </label>
+          </div>
+          <div className="form-group my-2">
+            <label>
+              Nhập lại mật khẩu:
+              <input
+                type="password"
+                placeholder="Enter password confirmation"
+                className="form-control"
+                name="password-confirmation"
+                id="password-confirmation"
+                onChange={(e) => setPassword_confirmation(e.target.value)}
+                required
+              />
+              {passworConfirmationErr ? (
+                <span style={{ color: "red" }}>Mậy khẩu không khớp</span>
+              ) : (
+                ""
+              )}
+            </label>
+          </div>
+          <div className="form-group my-2 form-check">
+            <label className="form-check-label">
+              <input
+                className="form-check-input"
+                type="checkbox"
+                id="gridCheck"
+                required
+              />
+              <p>
+                Tôi đã đọc và đồng ý các điều kiện
+                <Link to="/"> Thỏa thuận sử dụng</Link>.
+              </p>
+            </label>
+          </div>
+          <div className="form-group my-2">
+            <button type="submit" className="btn btn-danger btn-block btn-lg">
+              Đăng ký
+            </button>
+          </div>
+        </form>
+      </div>
+      <div className="register-footer">
+        <p>
+          Bạn đã có tài khoản rồi? <Link to="/user/login">Đăng nhập</Link>.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Register;
